832c0cc85dc4265cc06c02ab1ccc1121a770eeb6
iseruuuuu <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>
2023-06-04 22:04:07 +0900

    add: station17

--- ios-stations/BookAPIClient.swift
+++ ios-stations/BookAPIClient.swift
@@ -14,12 +14,14 @@ protocol BookAPIClientProtocol {
 class BookAPIClient: BookAPIClientProtocol {
     func fetchBooks(offset: Int, completion: @escaping ([Book]?) -> Void) {
         let urlString = "https://ifrbzeaz2b.execute-api.ap-northeast-1.amazonaws.com/public/books?offset=\(offset)"
+        //Alamofireを使用して、APIエンドポイントへのHTTPリクエストを送信する。
         AF.request(urlString).responseJSON { response in
             guard let data = response.data else {
                 completion(nil)
                 return
             }
             do {
+                //JSONDecoderを使用して、データをBookResponce型にデコードする。
                 let decoder = JSONDecoder()
                 let booksResponse = try decoder.decode(BooksResponse.self, from: data)
                 completion(booksResponse.books)
--- ios-stations/FirstViewController.swift
+++ ios-stations/FirstViewController.swift
@@ -16,19 +16,73 @@ class FirstViewController: UIViewController{
     
     @IBOutlet weak var tableView: UITableView!
     var bookAPIClient: BookAPIClientProtocol = BookAPIClient()
+    let refreshControl = UIRefreshControl()
+    var activityIndicator: UIActivityIndicatorView = UIActivityIndicatorView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        refreshControl.addTarget(self, action: #selector(refreshTableView), for: UIControl.Event.valueChanged)
+        tableView.refreshControl = refreshControl
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        fetchBooksFromAPI()
+    }
+    
+    //APIクライアントを使用して書籍データを非同期にFetchし、成功した場合は、TableViewの更新、失敗した場合は、エラーダイアログを出す。
+    func fetchBooksFromAPI() {
+        showLoading()
         bookAPIClient.fetchBooks(offset: 10) { [weak self] fetchedBooks in
+            DispatchQueue.main.async {
+                self?.stopLoading()
+            }
+            
             guard let fetchedBooks = fetchedBooks else {
-                print("Failed to fetch books.")
+                DispatchQueue.main.async {
+                    self?.showErrorAlert()
+                }
                 return
             }
+            
             DispatchQueue.main.async {
                 self?.books = fetchedBooks
+                self?.tableView.reloadData()
             }
         }
+    }
+    
+    @objc func refreshTableView() {
+        //変化が確認できるように一度リストを空にする。
+        books?.removeAll()
+        tableView.refreshControl?.endRefreshing()
+        tableView.reloadData()
+        fetchBooksFromAPI()
+    }
+    
+    @IBAction func fetchBooks(_ sender: Any) {
+        refreshTableView()
+    }
+    
+    func showErrorAlert() {
+        let alert = UIAlertController(title: "Error", message: "Failed to fetch books.", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default))
+        DispatchQueue.main.async {
+            self.present(alert, animated: true)
+        }
+    }
+    
+    func showLoading() {
+        activityIndicator.center = self.view.center
+        activityIndicator.hidesWhenStopped = true
+        activityIndicator.style = UIActivityIndicatorView.Style.large
+        view.addSubview(activityIndicator)
+        activityIndicator.startAnimating()
+        view.isUserInteractionEnabled = false
+    }
+    
+    func stopLoading() {
+        activityIndicator.stopAnimating()
+        view.isUserInteractionEnabled = true
     }
 }
 
@@ -43,7 +97,9 @@ extension FirstViewController: UITableViewDataSource {
         let book =  books[indexPath.row]
         cell.images.image = UIImage(named: "Book")
         cell.text1.text = book.title
-        cell.text2.text = book.id
+        cell.text2.text = book.detail
+        cell.text2.numberOfLines = 3
+        cell.text2.sizeToFit()
         return cell
     }
     
